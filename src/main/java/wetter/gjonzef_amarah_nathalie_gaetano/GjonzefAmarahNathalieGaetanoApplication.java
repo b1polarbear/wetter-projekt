28cd29c4e84d5f3962d9e7dae6e04bd99311a85c
Amarah Warren <150250332+b1polarbear@users.noreply.github.com>
2024-06-28 11:23:35 +0200

    Add files via upload

---
+++ src/main/java/wetter/gjonzef_amarah_nathalie_gaetano/GjonzefAmarahNathalieGaetanoApplication.java
@@ -0,0 +1,13 @@
+package wetter.gjonzef_amarah_nathalie_gaetano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjonzefAmarahNathalieGaetanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjonzefAmarahNathalieGaetanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wetter/gjonzef_amarah_nathalie_gaetano/GjonzefAmarahNathalieGaetanoApplicationTests.java
@@ -0,0 +1,13 @@
+package wetter.gjonzef_amarah_nathalie_gaetano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjonzefAmarahNathalieGaetanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
